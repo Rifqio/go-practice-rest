514dfc6e89a790435e1b78f4568a0367d34c9977
Rifqi Oktario <oktariorifqi@gmail.com>
2024-01-27 16:00:31 +0700

    fix: removing unecessary response header

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -6,11 +6,9 @@ import "net/http"
 // https://owasp.org/www-project-secure-headers/index.html#configuration-proposal
 func secureHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
-		res.Header().Set("Content-Security-Policy", "default-src 'self'; script-src 'self';")
 		res.Header().Set("X-Content-Type-Options", "nosniff")
-		res.Header().Set("Referrer-Policy", "origin-when-cross-origin")
 		res.Header().Set("X-Frame-Options", "deny")
-		res.Header().Set("X-XSS-Protection", "1; mode=block")
+		res.Header().Set("Content-Security-Policy", "frame-ancestors 'none'")
 
 		next.ServeHTTP(res, req)
 	})
